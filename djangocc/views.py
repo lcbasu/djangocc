71c9ba85143d132c1970688edd018066132f5e9a
Lokesh Chandra Basu <lokesh.basu@gmail.com>
2018-06-03 12:39:25 +0530

    refactor: Redesign home page

--- djangocc/views.py
+++ djangocc/views.py
@@ -6,15 +6,8 @@ def home(request):
     :param request:
     :return:
     '''
-    message = "Hope and necessity. They should the pillar of your future endeavours."
-    random_message_var = "Hello world!"
-
-    days_of_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-    # a dictionary with the keyword  'greeting_message' mapping to the variable 'message' defined above
-    # and other variables as well
+    # a dictionary with the keyword to be passed on as dictionary to the template for rendering
     context = {
-        'greeting_message': message,
-        'random_message': random_message_var,
-        'weekday_list': days_of_week,
-    } # to be passed on as dictionary to the template for rendering
+
+    }
     return render(request, 'home.html', context)
