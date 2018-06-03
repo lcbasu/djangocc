d485097dd2a032e240ddca0a5a3d157175d4cdeb
Lokesh Chandra Basu <lokesh.basu@gmail.com>
2018-06-03 11:37:33 +0530

    feat: Pass list in context and show it in template iteratively

--- djangocc/views.py
+++ djangocc/views.py
@@ -9,10 +9,12 @@ def home(request):
     message = "Hope and necessity. They should the pillar of your future endeavours."
     random_message_var = "Hello world!"
 
+    days_of_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     # a dictionary with the keyword  'greeting_message' mapping to the variable 'message' defined above
     # and other variables as well
     context = {
         'greeting_message': message,
-        'random_message':random_message_var
+        'random_message': random_message_var,
+        'weekday_list': days_of_week,
     } # to be passed on as dictionary to the template for rendering
     return render(request, 'home.html', context)
