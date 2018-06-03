cd228dad8f6d9fc93f08bc401cf9f90f8749157a
Lokesh Chandra Basu <lokesh.basu@gmail.com>
2018-06-03 13:29:03 +0530

    feat: Create first model for storing Deck info

--- flashcards/models.py
+++ flashcards/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Deck(models.Model):
+    title = models.CharField(max_length=64, null=False, blank=False)
+    description = models.CharField(max_length=256, null=False, blank=True)
