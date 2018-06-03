bc9939ec22f9b40ff691e9190f50f1565255fd78
Lokesh Chandra Basu <lokesh.basu@gmail.com>
2018-06-03 11:21:20 +0530

    feat: Pass variables to templates using context

--- djangocc/views.py
+++ djangocc/views.py
@@ -6,5 +6,13 @@ def home(request):
     :param request:
     :return:
     '''
-    context = {} # an empty dictionary
+    message = "Hope and necessity. They should the pillar of your future endeavours."
+    random_message_var = "Hello world!"
+
+    # a dictionary with the keyword  'greeting_message' mapping to the variable 'message' defined above
+    # and other variables as well
+    context = {
+        'greeting_message': message,
+        'random_message':random_message_var
+    } # to be passed on as dictionary to the template for rendering
     return render(request, 'home.html', context)
